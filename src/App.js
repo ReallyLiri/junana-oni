472710fe913b4418b6e36ca663de9375cd27f3b8
ReallyLiri <reallyliri@gmail.com>
2020-09-18 16:12:22 +0300

    loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
+import {SpinnerRoundFilled} from 'spinners-react';
 
-const Image = styled.img`
+const StyledImage = styled.img`
     display: table;
     margin: 0 auto;
     width: 720px;
     max-width: 100%;
+`;
+
+const StyledSpinner = styled(SpinnerRoundFilled)`
+  margin-top: 100px;
+  color: #B744B8 !important;
 `;
 
 const Images = [
@@ -17,11 +23,31 @@ const Images = [
     "https://i.imgur.com/Qr95Xwp.png"
 ];
 
+
+const loadImage = path =>
+    new Promise(resolve => {
+        const img = new Image();
+        img.onload = () => resolve(path);
+        img.src = path;
+    });
+
 const App = () => {
     const [imageIndex, setImageIndex] = useState(0);
+    const [isLoading, setLoading] = useState(true);
 
+    useEffect(() => {
+        Promise.all(Images.map(loadImage))
+            .then(
+                () => setLoading(false),
+                err => console.error(err)
+            )
+    }, []);
+
+    if (isLoading) {
+        return <StyledSpinner size={200}/>
+    }
     return <div onClick={() => setImageIndex(imageIndex + 1)}>
-        <Image
+        <StyledImage
             alt="image"
             src={Images[imageIndex % Images.length]}
         />
